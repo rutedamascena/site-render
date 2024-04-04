1acf542a1575463259cf2401d802fb60730d7ea9
rutedamascena <144967726+rutedamascena@users.noreply.github.com>
2024-04-03 21:58:48 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,3 +4,21 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
   return "<p>Hello, World!</p>"
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Clipping Automatizado</title>
+</head>
+<body>
+    <h1>Clipping Automatizado</h1>
+    <form action="/submit" method="post">
+        <label for="termo">Termo de Busca:</label><br>
+        <input type="text" id="termo" name="termo"><br>
+        <label for="email">Seu E-mail:</label><br>
+        <input type="email" id="email" name="email"><br><br>
+        <input type="submit" value="Buscar">
+    </form>
+</body>
+</html>
